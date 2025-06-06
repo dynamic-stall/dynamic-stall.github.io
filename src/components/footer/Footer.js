37db42fdbe1a7bfcd07bf151dfbbf68d7a458e0c
Henry D. Evon <dchenry006@gmail.com>
2025-06-06 01:54:25 -0400

    import seo elements from portfolio for footer text

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./Footer.css";
 import { Fade } from "react-reveal";
-import { greeting } from "../../portfolio.js";
+//import { greeting } from "../../portfolio.js";
+import { seo } from "../../portfolio.js";
 /* eslint-disable jsx-a11y/accessible-emoji */
 
 export default function Footer(props) {
@@ -11,7 +12,8 @@ export default function Footer(props) {
     <div className="footer-div">
       <Fade>
         <p className="footer-text" style={{ color: props.theme.secondaryText }}>
-          © {currentYear} Triskelion Flagship, LLC
+          {/*Made with <span role="img">❤️</span> by {greeting.title}*/}
+          © {currentYear} {seo.title}
         </p>
         {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle}/> */}
       </Fade>
