0e8d08fb95ff82305ee5749ffeac802ee9d02008
Henry D. Evon <dchenry006@gmail.com>
2025-06-06 01:42:27 -0400

    update footer with LLC info

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,11 +5,13 @@ import { greeting } from "../../portfolio.js";
 /* eslint-disable jsx-a11y/accessible-emoji */
 
 export default function Footer(props) {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="footer-div">
       <Fade>
         <p className="footer-text" style={{ color: props.theme.secondaryText }}>
-          Made with <span role="img">❤️</span> by {greeting.title}
+          © {currentYear} Triskelion Flagship, LLC
         </p>
         {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle}/> */}
       </Fade>
