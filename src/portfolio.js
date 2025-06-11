19b928b12a542fe5f1625e7b78c5502e213e7596
dynamic-stall <dchenry006@gmail.com>
2025-06-11 01:30:17 -0400

    maximize opacity for logo background colors (certs)

--- src/portfolio.js
+++ src/portfolio.js
@@ -406,7 +406,7 @@ const certifications = {
       certificate_link:
         "https://www.certmetrics.com/amazon/electronic_certificate.aspx?cert=9532A418C09A90F4C4E0CC3705E2C42FOE831595D07ABB8C7EA0DD4C480A7F79E",
       alt_name: "aws1",
-      color_code: "#FF910020", // light orange
+      color_code: "#FF9100FF", // light orange
     },
     {
       title: "Network+",
@@ -424,7 +424,7 @@ const certifications = {
       certificate_link:
         "https://certification.scrumalliance.org/accounts/875481-devon-charles-henry/certifications?",
       alt_name: "scrum",
-      color_code: "#1E3A8A80", // navy blue
+      color_code: "#1E3A8AFF", // navy blue
     },
     {
       title: "Git and GitHub Bootcamp",
@@ -433,7 +433,7 @@ const certifications = {
       certificate_link:
         "https://www.udemy.com/certificate/UC-4241213d-4aab-4adf-a5e6-34f5100eda62/",
       alt_name: "udemy1",
-      color_code: "#A020F020", // light purple
+      color_code: "#A020F0FF", // light purple
     },
     {
       title: "Complete Python Bootcamp",
@@ -442,7 +442,7 @@ const certifications = {
       certificate_link:
         "https://www.udemy.com/certificate/UC-2698df4b-366f-4e18-9316-f88c9a6945af/",
       alt_name: "udemy2",
-      color_code: "#A020F020", // light purple
+      color_code: "#A020F0FF", // light purple
     },
     {
       title: "AWS Partner: Technical Accreditation",
@@ -451,7 +451,7 @@ const certifications = {
       certificate_link:
         "https://www.credly.com/badges/5a69529c-65e1-4c4c-ac6d-403e66aafe36?source=linked_in_profile",
       alt_name: "aws2",
-      color_code: "#FF910020", // light orange
+      color_code: "#FF9100FF", // light orange
     },
     {
       title: "Kubernetes Essentials",
@@ -469,7 +469,7 @@ const certifications = {
       certificate_link:
         "https://scaledagile.com/certification/",
       alt_name: "safe4",
-      color_code: "#20B2AA25", // light sea green
+      color_code: "#20B2AAFF", // light sea green
     },
     {
       title: "Certified Cloud Practitioner",
@@ -478,7 +478,7 @@ const certifications = {
       certificate_link:
         "https://www.certmetrics.com/amazon/electronic_certificate.aspx?cert=8455114F519DF416C1A108F5245B0046O8D1BB2BD3A9806FB4E8BB129FFF5F5F0",
       alt_name: "aws3",
-      color_code: "#FF910020", // light orange
+      color_code: "#FF9100FF", // light orange
     },
     {
       title: "IoT-Inc Certified IoT Professional",
@@ -487,7 +487,7 @@ const certifications = {
       certificate_link:
         "https://www.credential.net/qghx52ba",
       alt_name: "iotinc",
-      color_code: "#FFD70025", // light gold
+      color_code: "#FFD700FF", // light gold
     },
   ],
 };
@@ -584,7 +584,7 @@ const experience = {
           duration: "October 2014 - July 2017",
           location: "Brambleton, VA",
           description:
-            "I worked primarily in a volunteer capacity for this after-school STEM mentorship program catering to girls in grades 3-12. I primarily focused on WordPress site maintenance, social media outreach (developing and presenting a Social Media Business Plan to organizational stakeholders), and booth management during events and conferences.",
+            "I worked primarily in a volunteer capacity for this after-school STEM mentorship program catering to girls in grades 6-12. I primarily focused on WordPress site maintenance, social media outreach (developing and presenting a Social Media Business Plan to organizational stakeholders), and booth management during events and conferences.",
           color: "#181717",
         },
       ],
