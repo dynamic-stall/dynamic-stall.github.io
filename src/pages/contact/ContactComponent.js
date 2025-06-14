31b59e9bacaed94a9d31fc1b5450d45d9389f7e3
dynamic-stall <dchenry006@gmail.com>
2025-06-13 20:18:46 -0400

    increase profile image size

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -28,7 +28,7 @@ class Contact extends Component {
                 <img
                   src={require(`../../assets/images/${ContactData["profile_image_path"]}`)}
                   alt="Profile"
-                  style={{ width: "200px", height: "200px", objectFit: "cover", borderRadius: "50%" }}
+                  style={{ width: "300px", height: "300px", objectFit: "cover", borderRadius: "50%" }}
                 />
               </div>
               <div className="contact-heading-text-div">
