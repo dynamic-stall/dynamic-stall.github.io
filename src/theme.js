03c003cfa36a7853e376f12698b764e9e4733125
dynamic-stall <dchenry006@gmail.com>
2025-06-15 13:31:36 -0400

    update 'flagshipTealTheme' highlight color

--- src/theme.js
+++ src/theme.js
@@ -231,7 +231,7 @@ export const flagshipTealTheme = {
   body: "#ffffff",
   text: "#025C5C",
   expTxtColor: "#000000",
-  highlight: "#278a86",
+  highlight: "#30b8b2",
   dark: "#07292c",
   secondaryText: "#000000",
   imageHighlight: "#0a343c",
