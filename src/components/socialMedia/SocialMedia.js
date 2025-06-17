a87b9a8cf1e64deb69a4299b9eda9d715b539135
dynamic-stall <dchenry006@gmail.com>
2025-06-17 15:09:22 -0400

    refactor social media icon imports (react-component > CSS)

--- src/components/socialMedia/SocialMedia.js
+++ src/components/socialMedia/SocialMedia.js
@@ -4,6 +4,7 @@ import { socialMediaLinks } from "../../portfolio";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+// Styled wrapper with backgroundColor hover logic
 const IconWrapper = styled.span`
   i {
     background-color: ${(props) => props.backgroundColor};
@@ -13,6 +14,31 @@ const IconWrapper = styled.span`
     transition: 0.3s ease-in;
   }
 `;
+
+// Helper to determine icon prefix based on known brand icons
+function getFAIcon(iconString) {
+  if (!iconString) return null;
+
+  const iconName = iconString.replace("fa-", "");
+
+  // Extend this list if you use more brand icons
+  const brandIcons = [
+    "github",
+    "linkedin-in",
+    "youtube",
+    "tiktok",
+    "google",
+    "facebook",
+    "instagram",
+    "x-twitter",
+    "discord",
+    "whatsapp",
+  ];
+
+  const prefix = brandIcons.includes(iconName) ? "fab" : "fas";
+
+  return [prefix, iconName];
+}
 
 export default function SocialMedia(props) {
   return (
@@ -26,7 +52,7 @@ export default function SocialMedia(props) {
           rel="noopener noreferrer"
         >
           <IconWrapper {...media} {...props}>
-            <FontAwesomeIcon icon={media.fontAwesomeIcon} />
+            <FontAwesomeIcon icon={getFAIcon(media.fontAwesomeIcon)} />
           </IconWrapper>
         </a>
       ))}
