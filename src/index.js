2aacd04f0d8b57936d8cb6c55539a6a94731fccc
dynamic-stall <dchenry006@gmail.com>
2025-06-17 16:07:18 -0400

    fix typo in icon import

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import "./assets/font-awesome/css/all.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 //import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faEnvelope } from "@fontawesome/free-regular-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 library.add(fab, faEnvelope);
 
