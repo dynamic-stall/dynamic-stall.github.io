99e1e4bbacedb19dc95a42905132b2965b5c6cf2
Henry D. Evon <dchenry006@gmail.com>
2025-07-06 01:54:09 -0400

    change nav bar text for contact page link

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -130,7 +130,7 @@ class Header extends Component {
                   onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}
                   onMouseOut={(event) => onMouseOut(event)}
                 >
-                  Get in Touch
+                  Connect
                 </NavLink>
               </li>
             </ul>
