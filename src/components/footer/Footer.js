b6a9465cc160d7df375e9bca2e3e13ad8f5e85fa
Henry D. Evon <dchenry006@gmail.com>
2025-07-12 16:50:54 -0400

    comment-out unused definitions

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./Footer.css";
 import { Fade } from "react-reveal";
 //import { greeting } from "../../portfolio.js";
-import { seo } from "../../portfolio.js";
+//import { seo } from "../../portfolio.js";
 /* eslint-disable jsx-a11y/accessible-emoji */
 
 export default function Footer(props) {
-  const currentYear = new Date().getFullYear();
+  //const currentYear = new Date().getFullYear();
 
   return (
     <div className="footer-div">
